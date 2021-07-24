6c9bbda6f3fccd0a71e1ae67fe04735a4e6bf688
Andres Felipe Sierra Rojas <79063920+andres06-hub@users.noreply.github.com>
2021-07-23 21:15:25 -0500

    Update archivo_1.py

--- archivo_1.py
+++ archivo_1.py
@@ -1,6 +1,18 @@
-# HOLAAAAAAAAAAAAAAAA
-# añañañañaañañayyyy
-# JEJEJJEJEJEJEJEJJEJEEJJEJJEJEJJEJ
-n1 = 8854
-n2 = 45
-s = n1 + n2 
+
+# HOLAAAA
+# Que sencilloooo
+
+def ingresa_n1():
+  return float(input("Ingreseun numero: ")
+
+def ingresa_n2():
+  return float(input("Ingreseun numero: ")     
+               
+def operacion_suma (n1,n2):
+  return resul = n1 + n2
+#----------------------
+               
+n1 = ingresa_n1()
+n2 = ingresa_n2()
+suma = operacion_suma()
+print(f"La suma es = {suma}")
